20b9803f05fddbb1476e468dc63e3d0371abdb08
Thomas Levine <_@thomaslevine.com>
2015-08-10 14:04:45 -0400

    remove __iter__ test

--- kfij/test/test_set.py
+++ kfij/test/test_set.py
@@ -7,7 +7,8 @@ from ..kfijset import Set
 
 # @pytest.mark.randomize(func, ncalls=10)
 
-method_calls = itertools.product(set(dir(Set)) - set(dir(Kfij)), [tuple(), ('abc',)])
+method_names = set(dir(Set)) - set(dir(Kfij)) - {'__iter__'}
+method_calls = itertools.product(method_names, [tuple(), ('abc',)])
 
 @pytest.mark.parametrize('method_name, args', method_calls)
 def test_method_call(method_name, args):
