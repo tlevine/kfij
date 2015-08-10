423ebc24931f6806835e6c3e69524d0db4c35c7e
Thomas Levine <_@thomaslevine.com>
2015-08-10 09:10:05 -0400

    copy set functions

--- kfij/_set.py
+++ kfij/_set.py
@@ -3,40 +3,48 @@ import os
 
 class Set:
     '''
-    Persist a set-like structure to a file.
+    Persist an append-only set-like structure to a file.
     The contents of the set must be str or bytes.
     '''
-    def __init__(self, filename, binary_mode = False):
+    def __init__(self, filename):
         '''
         :param str filename: File at which to save state
-        :param bool binary_mode: Whether the file should be opened in binary mode
-            and, in turn, whether you are going to add bytes or str to the set
         '''
-        b = 'b' if binary_mode else ''
 
         if os.path.exists(filename):
             with open(filename, 'r') as fp:
-                original_data = set(line.rstrip('\r\n') for line in fp)
+                self._set = set(line.rstrip('\r\n') for line in fp)
         else:
             with open(filename, 'w') as fp:
                 fp.write('')
-            original_keys_to_skip = set()
+            self._set = set()
 
-        keys_to_skip = set(original_keys_to_skip)
-        for x in xs:
-            key, _ = x
+        self._fp = open(filename, 'a')
 
-            # Validate key
-            if not isinstance(key, str) or '\r' in key or '\n' in key:
-                raise NotImplementedError('Can\'t handle this sort of key')
+    def add(self, x):
+        '''
+        :param str x: Item to be added to the set
+        '''
+        if not isinstance(x, str) or '\r' in key or '\n' in key:
+            raise NotImplementedError('Can\'t handle this sort of value')
 
-            # Filter
-            if key in keys_to_skip:
-                logger.debug('Skipping key "%s" from %s' % (key, xs))
-            else:
-                keys_to_skip.add(key)
-                yield x
+        # Filter
+        if x not in self._x:
+            self._set.add(key)
+            self._fp.write(key + '\n')
 
-        with open(filename, 'a') as fp:
-            for key in keys_to_skip - original_keys_to_skip:
-                fp.write(key + '\n')
+    def clear(self):
+    def copy(self):
+        discard
+    intersection_update
+    pop
+    remove
+    symmetric_difference_update
+    update
+
+safe_func_names =  [
+    'difference', 'union', 'intersection', 'symmetric_difference',
+    'isdisjoint', 'issubset',
+]
+for func_name in safe_func_names:
+    setattr(Set, func_name, lambda self, iterable: getattr(self._set, func_name)(iterable))
