2ac70c2a84f7aee9608a109613d20bbc82326276
Thomas Levine <_@thomaslevine.com>
2015-08-10 10:48:43 -0400

    Set

--- kfij/kfij.py
+++ kfij/kfij.py
@@ -67,8 +67,6 @@ class Kfij:
         that the present object is empty before load is called.
         '''
         raise NotImplementedError('You must set %s.load.', self.__class__.__name__)
-        for line in fp:
-            self.append(line.rstrip('\r\n'))
 
     def dump(self, fp):
         '''
@@ -76,8 +74,6 @@ class Kfij:
         object in a form that the load function can parse.
         '''
         raise NotImplementedError('You must set %s.dump.', self.__class__.__name__)
-        for x in self:
-            fp.write(x + '\n')
 
     @classmethod
     def apply_destructive_funcs(Class, *func_names):
@@ -116,12 +112,22 @@ class Kfij:
 
             setattr(Class, func_name, g)
         
-
-safe_func_names =  [
-    'copy',
-    'difference', 'union', 'intersection', 'symmetric_difference',
-    'isdisjoint', 'issubset',
-]
-destructive_func_names = [
-
-
+class Set(Kfij):
+    factory = set
+    def load(self, fp):
+        for line in fp:
+            self.add(line.rstrip('\r\n'))
+    def dump(self, fp):
+        for x in self:
+            fp.write(x + '\n')
+Set.enable_safe_funcs('copy', 'isdisjoint', 'issubset',
+                      'difference', 'union', 'intersection', 'symmetric_difference',
+                      '__contains__', '__len__',
+                      '__sub__', '__rsub__',
+                      '__and__', '__or__',
+                      '__rand__', '__ror__', '__rxor__',
+                      '__ge__', '__gt__', '__le__', '__lt__', '__eq__')
+Set.enable_destructive_funcs('clear', 'pop', 'remove',
+                             'intersection_update',
+                             'symmetric_difference_update',
+                             'update')
